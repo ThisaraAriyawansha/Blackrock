605d054752e45f0dbfd4e508cb21ac0dc315e0af
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-10-13 13:12:16 +0530

    update

--- blackrock/src/components/Navbar.js
+++ blackrock/src/components/Navbar.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import './CSS/Navbar.css'; // Importing the CSS file
 
-
 const Navbar = () => {
-    return (
-      <div>
-        {/* Navbar */}
-        <nav className="navbar">
-          <div className="logo">BlackRock</div>
-          <ul>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Newsroom</a></li>
-            <li><a href="#">Insights</a></li>
-            <li><a href="#">Investor Relations</a></li>
-            <li><a href="#">Careers</a></li>
-          </ul>
-        </nav>
+  return (
+    <div>
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="logo">BlackRock</div>
+        <ul>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Newsroom</a></li>
+          <li><a href="#">Insights</a></li>
+          <li><a href="#">Investor Relations</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+        {/* Search bar */}
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-      </div>
-    );
-  };
-  
-  export default Navbar;
-  
+export default Navbar;
