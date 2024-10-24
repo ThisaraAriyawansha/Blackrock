f031a7867a296877dff6cf902f21a193658f4387
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-10-24 12:23:07 +0530

    update

--- blackrock/src/App.js
+++ blackrock/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import SmallNavbar from './components/SmallNavBar'
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/navbar" element={<Navbar />} />
+          <Route path="/footer" element={<Footer />} />
           <Route path="/smallNavbar" element={<SmallNavbar />} />
 
         </Routes>
