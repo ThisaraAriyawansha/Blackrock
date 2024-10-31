45b910a81398e1b4afcd832d8cad1fedaf3fab49
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-10-31 12:19:37 +0530

    updates

--- blackrock/src/components/SmallNavBar.js
+++ blackrock/src/components/SmallNavBar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CSS/SmallNavBar.css'; 
 
 const HorizontalNavBar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="horizontal-nav-container">
       <div className="left-links">
@@ -11,8 +17,19 @@ const HorizontalNavBar = () => {
         <a href="/">Our company</a>
       </div>
       <div className="right-link">
-        <a href="/local-websites">Local websites</a>
+        {/* Use Font Awesome icon */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <i className="fas fa-ellipsis-v"></i> {/* Font Awesome icon */}
+        </div>
       </div>
+      {isMobileMenuOpen && (
+        <div className="mobile-menu">
+          <a href="/">BlackRock</a>
+          <a href="/ishares">iShares</a>
+          <a href="/aladdin">Aladdin</a>
+          <a href="/">Our company</a>
+        </div>
+      )}
     </nav>
   );
 };
